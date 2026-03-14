775ac72db60858929ba7f566116893b12b13a8e8
VXPhuc <xuanphucvo19@gmail.com>
2026-03-14 21:48:40 +0700

    add api production

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,6 @@
 
 export const config = {
   apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  apiUrlProduction: import.meta.env.VITE_API_URL_PRODUCTION || 'https://geneology-web-be-tke0.onrender.com/api',
   environment: import.meta.env.MODE,
 }
